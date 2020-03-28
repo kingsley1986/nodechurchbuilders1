4120a3b5d91b10886a96a461d8613933fbc638ed
Kingsley <chukwumakingsley1@gmail.com>
2020-03-28 18:12:12 +0200

    comment now working

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,27 @@ const BlogPost = new Schema({
   title: String,
   body: String,
   date: { type: Date, default: Date.now },
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+
+
+});
+
+const BlogComment = new Schema({
+  body  : {type: String, required: true},
+  post: [{ type: Schema.Types.ObjectId, ref: 'Post'}],
+
+  date: { type: Date, default: Date.now },
 });
 
 var Post = mongoose.model('Post', BlogPost);
+var Comment = mongoose.model('Comment', BlogComment);
 
 // Get new post form
 router.get('/newpost', function(req, res, next) {
   res.render("newchurchpost");
 });
 
-
+//create a post
 router.post("/makepost", (req, res) => {
 
   var myData = new Post(req.body);
@@ -32,11 +43,27 @@ router.post("/makepost", (req, res) => {
   .catch(err => {
     res.status(400).send("unable to save to database");
   });
+});
 
+// CREATE Comment
+router.post("/posts/:postId/comment", async(req, res) => {
+  const post = await Post.findOne({_id: req.params.postId});
+
+  const comment = new Comment();
+  comment.body = req.body.body;
+  comment.post = post._id;
+  await comment.save();
+
+  post.comments.push(comment._id);
+  await post.save();
+
+  res.render('post_show',{
+    "post": post
+  });
 });
 
 
-// Get a Post
+// Get Posts
 router.get('/posts', function(req, res, next) {
   Post.find()
     .then(function(doc) {
@@ -44,7 +71,43 @@ router.get('/posts', function(req, res, next) {
     });
 });
 
+
+// Get post with comments
+router.get("/post/:postId/comments", async (req, res) => {
+  const post = await Post.findOne({_id: req.params.postId}).populate(
+    "comments"
+  );
+  res.render('post_show',{
+    "post": post
+  });
+})
+
+// Get a post
+// router.get('/post/:id', function(req, res, next) {
+//   Post.findById(req.params.id, function(err, post){
+//     res.render('post_show',{
+//       "post": post
+//     });
+//   });
+// });
+
+
+
 // Delete post
+
+// router.get('/post/:id', function(req, res, next) { 
+
+//   Post.find({})
+//   .populate('commentsIds')
+//   .exec(function(err, posts, count){  
+//     res.render( 'post_show', {
+//       user: req.user,
+//       page : 'post_show',
+//       title : '??????',
+//       posts : posts
+//     });
+// });
+// });
 
 
 router.delete( '/post/:id', function( req, res ){
