60c9569761fc10b61c5ebe6b434337c71f9c44fe
Kingsley <chukwumakingsley1@gmail.com>
2020-05-25 19:23:17 +0200

    removed envi variable

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var usersRouter = require('./routes/users');
 var eventcommentRouter = require('./routes/event_comments');
 var contactformRouter = require('./routes/contact_forms');
 
-mongoose.connect(process.env.DATABASE_URL || process.env.MONGODB_URI, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
