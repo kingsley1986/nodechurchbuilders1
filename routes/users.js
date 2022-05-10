5f7d0868dc32eecc6d12d47ea87510eb5bd364b7
kingsley <chukwumakingsley1@gmail.com>
2022-05-10 22:53:16 +0200

    changes

--- routes/users.js
+++ routes/users.js
@@ -1,143 +1,153 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user');
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var User = require("../models/user");
 
 // handle file uploads
-const fs = require('fs')
-const multer  = require('multer')
-const path =  require('path')
-const uploadPath = path.join('public', User.userImageBasePath)
-const imageMineTypes = ['image/jpeg', 'image/png', 'image/gif']
+const fs = require("fs");
+const multer = require("multer");
+const path = require("path");
+const uploadPath = path.join("public", User.userImageBasePath);
+const imageMineTypes = ["image/jpeg", "image/png", "image/gif"];
 
-const upload = multer({ 
+const upload = multer({
   dest: uploadPath,
-  fileFilter:  (req, file, callback) => {
-    callback(null, imageMineTypes.includes(file.mimetype) )
-  }
-})
-
-
+  fileFilter: (req, file, callback) => {
+    callback(null, imageMineTypes.includes(file.mimetype));
+  },
+});
 
 /* GET users listing. */
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const users =  await User.find({})
-    res.render('showusers', {
-      users: users
+    const users = await User.find({});
+    res.render("showusers", {
+      users: users,
     });
   } catch {
-    res.redirect('/')
+    res.redirect("/");
   }
 });
 
-router.get('/register', function(req, res, next) {
-  res.render('register', {title: 'Register'});
+router.get("/register", function (req, res, next) {
+  res.render("register", { title: "Register", layout: false });
 });
-router.get('/login', function(req, res, next) {
-  res.render('login', {title: 'Login'});
-});
-
-router.post('/login',
-  passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: 'Invalid username or password'}),
-  function(req, res) {
-   req.flash('success', 'You are now logged');
-   res.redirect('/');
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "Login", layout: false });
 });
 
-passport.serializeUser(function(user, done) {
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/users/login",
+    failureFlash: "Invalid username or password",
+  }),
+  function (req, res) {
+    req.flash("success", "You are now logged");
+    res.redirect("/");
+  }
+);
+
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.getUserById(id, function(err, user) {
+passport.deserializeUser(function (id, done) {
+  User.getUserById(id, function (err, user) {
     done(err, user);
   });
 });
 
- passport.use(new LocalStrategy(function(username, password, done) {
-   User.getUserByUsername(username, function(err, user) {
-    if(err) throw err;
-    if(!user) {
-      return done(null, false, {message: 'Unknown User'});
-    }
-    User.comparePassword(password, user.password, function(err, isMatch) {
-      if(err) return done(err);
-      if(isMatch) {
-        return done(null, user);
-      }else {
-        return done(null, false, {message: 'Invalid Password'});
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.getUserByUsername(username, function (err, user) {
+      if (err) throw err;
+      if (!user) {
+        return done(null, false, { message: "Unknown User" });
       }
+      User.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) return done(err);
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Invalid Password" });
+        }
+      });
     });
-  });
-}));
+  })
+);
 
+router.post(
+  "/register",
+  upload.single("profileimage"),
+  function (req, res, next) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    if (req.file) {
+      console.log("Uploading File....");
+      var profileimage = req.file.filename;
+    } else {
+      console.log("No File Uploaded....");
+      var profileimage = "noimage.jpg";
+    }
 
+    // Form validator
+    req.checkBody("name", "Name field is required").notEmpty();
+    req.checkBody("email", "Email field is required").notEmpty();
+    req.checkBody("email", "Email field is required").isEmail();
+    req.checkBody("username", "Username field is required").notEmpty();
+    req.checkBody("password", "password field is required").notEmpty();
+    req
+      .checkBody("password2", "password do not match")
+      .equals(req.body.password);
 
-router.post('/register', upload.single('profileimage'), function(req, res, next) {
-  var name = req.body.name;
-  var email = req.body.email;
-  var username = req.body.username;
-  var password = req.body.password;
-  var password2 = req.body.password2;
-  if(req.file) {
-    console.log('Uploading File....');
-    var profileimage = req.file.filename;
-  }else {
-    console.log('No File Uploaded....');
-    var profileimage = 'noimage.jpg';
+    const fileName = req.file != null ? req.file.filename : null;
+
+    // check Errors
+    var errors = req.validationErrors();
+
+    User.findOne(
+      {
+        email: req.body.email,
+      },
+      function (err, user) {
+        if (user) {
+          req.flash("error", "user already exists", req.body.email);
+          res.render("register");
+        } else if (errors) {
+          res.render("register", {
+            errors: errors,
+          });
+        } else if (password.length < 6) {
+          req.flash("error", "password must be at least 6 characters");
+          res.render("register");
+        } else {
+          var newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password,
+            profileimage: fileName,
+          });
+          User.createUser(newUser, function (err, user) {
+            if (err) throw err;
+            console.log(user);
+          });
+          req.flash("success", "You are now registered and and login");
+          res.redirect("/");
+        }
+      }
+    );
   }
+);
 
-
-  // Form validator
-  req.checkBody('name', 'Name field is required').notEmpty();
-  req.checkBody('email', 'Email field is required').notEmpty();
-  req.checkBody('email', 'Email field is required').isEmail();
-  req.checkBody('username', 'Username field is required').notEmpty();
-  req.checkBody('password', 'password field is required').notEmpty(); 
-  req.checkBody('password2', 'password do not match').equals(req.body.password);
-
-
-  const fileName = req.file != null ? req.file.filename : null
-
-  // check Errors
-  var errors = req.validationErrors();
-
-  User.findOne({
-    email: req.body.email,
-    }, function (err, user) {
-    if (user) {
-      req.flash('error', 'user already exists',  req.body.email);
-      res.render('register');
-    }else if(errors) {
-        res.render('register', {
-        errors: errors
-        });
-    }else if (password.length < 6) {
-      req.flash('error', 'password must be at least 6 characters');
-      res.render('register');
-      }else {
-    var newUser = new User({
-      name: name,
-      email: email,
-      username: username,
-      password: password,
-      profileimage: fileName
-    });
-    User.createUser(newUser, function(err, user){
-      if(err) throw err;
-      console.log(user);
-    });
-      req.flash('success', 'You are now registered and and login');
-      res.redirect('/'); 
-    } 
-  });
-});
-
-router.get('/logout', function(req, res) {
+router.get("/logout", function (req, res) {
   req.logout();
-  req.flash('success', 'You are now Logged out');
-  res.redirect('/users/login');
+  req.flash("success", "You are now Logged out");
+  res.redirect("/users/login");
 });
 module.exports = router;
